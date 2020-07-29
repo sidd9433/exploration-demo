39783ec2118d8b232c2a187d825064afba1b4893
“Siddhartha <siddhartha.mukherjee@rabobank.nl>
2020-07-29 13:12:57 +0200

    first commit

---
+++ src/main/java/com/cloudboosters/exploration/demo/ExplorationDemoApplication.java
@@ -0,0 +1,12 @@
+package com.cloudboosters.exploration.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorationDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cloudboosters/exploration/demo/ExplorationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudboosters.exploration.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
